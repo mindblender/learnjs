094787e816afb51d3001813f57fa8df5e047300d
Jeff Sittler <mindblender@gmail.com>
2020-03-06 15:57:32 -0700

    changed up to app shell

--- public/app.js
+++ public/app.js
@@ -1,5 +1,16 @@
-'use strict'
-var learnjs = {};
+'use strict';
+let learnjs = {};
+
+learnjs.problems = [
+    {
+        description: "What is truth?",
+        code: "function problem() { return __; }"
+    },
+    {
+        description: "Simple Math",
+        code: "function problem() { return 42 === 6 * __; }"
+    }
+];
 
 learnjs.appOnReady = function () {
     window.onhashchange = function () {
@@ -9,18 +20,75 @@ learnjs.appOnReady = function () {
     learnjs.showView(window.location.hash);
 };
 
-learnjs.problemView = function (problemNumber) {
-    var $title = 'Problem #' + problemNumber + ' Coming soon!';
-    return $('<div class="problem-view">').text($title);
+learnjs.applyObject = function (obj, elem) {
+    for (let key in obj) {
+        elem.find('[data-name="' + key + '"]').text(obj[key]);
+    }
+};
+
+learnjs.problemView = function (data) {
+    let problemNumber = parseInt(data, 10);
+    let view = $('.templates .problem-view').clone();
+    let problemData = learnjs.problems[problemNumber - 1];
+    let resultFlash = view.find('.result');
+
+    function checkAnswer() {
+        let answer = view.find('.answer').val();
+        let test = problemData.code.replace('__', answer) + '; problem();';
+        return eval(test);
+    }
+
+    function checkAnswerClick() {
+        if (checkAnswer()) {
+            // let correctFlash = learnjs.template('correct-flash');
+            // correctFlash.find('a').attr('href', '#problem-' + (problemNumber + 1));
+            // learnjs.flashElement(resultFlash, correctFlash);
+            // learnjs.buildCorrectFlash(problemNumber);
+            learnjs.flashElement(resultFlash, learnjs.buildCorrectFlash(problemNumber))
+        } else {
+            learnjs.flashElement(resultFlash, 'Incorrect!');
+        }
+        return false;
+    }
+
+    view.find('.check-btn').click(checkAnswerClick);
+    view.find('.title').text('Problem #' + problemNumber);
+    learnjs.applyObject(problemData, view);
+    return view;
 };
 
 learnjs.showView = function (hash) {
-    var routes = {
+    let routes = {
         '#problem': learnjs.problemView
     };
-    var hashParts = hash.split('-');
-    var viewFn = routes[hashParts[0]];
+    let hashParts = hash.split('-');
+    let viewFn = routes[hashParts[0]];
     if (viewFn) {
         $('.view-container').empty().append(viewFn(hashParts[1]));
     }
+};
+
+learnjs.flashElement = function (elem, content) {
+    elem.fadeOut('fast', function () {
+        elem.html(content);
+        elem.fadeIn();
+    })
+};
+
+learnjs.buildCorrectFlash = function (problemNum) {
+    let correctFlash = learnjs.template('correct-flash');
+    let link = correctFlash.find('a');
+    console.log(problemNum);
+    console.log(learnjs.problems.length);
+    if (problemNum < learnjs.problems.length) {
+        link.attr('href', '#problem-' + (problemNum + 1));
+    } else {
+        link.attr('href', '');
+        link.text('You\'re Finished!');
+    }
+    return correctFlash;
+};
+
+learnjs.template = function (name) {
+    return $('.templates .' + name).clone();
 };
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -26,10 +26,26 @@ describe('LearnJS', function () {
         expect(learnjs.problemView).toHaveBeenCalledWith('42');
     });
 
+/*
     describe('problem view', function () {
         it('has a title that includes the problem number', function () {
             var view = learnjs.problemView('1');
             expect(view.text()).toEqual('Problem #1 Coming soon!');
         });
     });
+*/
+/*
+    describe('answer section', function () {
+        var view = learnjs.problemView('1');
+        it('can check a correct answer by hitting a button', function () {
+            view.find('.answer').val('true');
+            view.find('.check-btn').click();
+            expect(view.find('.result').test()).toEqual('Correct!');
+        });
+        it('rejects an incorrect answer', function () {
+            view.find('.answer').val('false');
+            view.find('.check-btn').click();
+            expec(view.find('.result').text()).toEqual('Incorrect!');
+        });
+    })*/
 });
